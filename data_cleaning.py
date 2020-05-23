6a3562e7627f24144f7073d3584c889b52b1b5e4
Ratnaraj <ratnarajwora@gmail.com>
2020-05-23 19:22:38 +0530

    cleaned data

--- data_cleaning.py
+++ data_cleaning.py
@@ -5,11 +5,9 @@ Created on Sat May 23 11:00:56 2020
 @author: Ratnaraj
 """
 import pandas as pd
+import numpy as np
 
 df = pd.read_csv('raw_data.csv')
-
-
-
 df = df[df['Salary Estimate'] != '-1']
 df.drop('Unnamed: 0',axis=1,inplace=True)
 
@@ -23,9 +21,10 @@ df['max_salary'] = minus_kd.apply(lambda x: int(x.split('-')[1]))
 df['avg_salary'] = (df['min_salary']+df['max_salary'])/2
 
 
-# Company name text only
 
+# Company name text only
 df['company_txt'] = df.apply(lambda x: x['Company Name'] if x['Rating'] <0 else x['Company Name'][:-3] , axis =1 )
+
 
 
 # State Field 
@@ -37,8 +36,13 @@ for i in range(df.shape[0]):
     
 df['same_state'] = df.apply(lambda x: 1 if x.Location == x.Headquarters else 0,axis=1)
 
+df['state_txt'] = df.state_txt.apply(lambda x: x.strip() if x.strip().lower() != 'new jersey' else 'NJ')
+df['state_txt'] = df.state_txt.apply(lambda x: x.strip() if x.strip().lower() != 'virginia' else 'VA')
+df['state_txt'] = df.state_txt.apply(lambda x: x.strip() if x.strip().lower() != 'oregeon' else 'OR')
 # Age of Company 
 df['age_company'] = df['Founded'].apply(lambda x: 2020-x if x>0 else -1)
+
+
 
 # Parsing of job description (Python, R-studio) 
 
@@ -61,6 +65,65 @@ df['aws_yn'] = df['Job Description'].apply(lambda x: 1 if 'aws' in x.lower() els
 #excel
 df['excel_yn'] = df['Job Description'].apply(lambda x: 1 if 'excel' in x.lower() else 0 )
 #print(df.excel_yn.value_counts())
+
+
+
+#Title
+for i in range(df.shape[0]):
+    title = df.loc[i,'Job Title']
+    #Data Scientist,scientist
+    if 'data sciencetist' in title.lower():
+        df.loc[i,'job_simp'] = 'data scientist'        
+    
+    #Data Analyst
+    elif 'data engineer' in title.lower():
+        df.loc[i,'job_simp'] = 'data engineer'        
+    
+    #Data engineer
+    elif 'analyst' in title.lower():
+        df.loc[i,'job_simp'] = 'analyst'        
+    
+    #Machine Learning
+    elif 'machine learning' in title.lower():
+        df.loc[i,'job_simp'] = 'mle'        
+    
+    #Manager
+    elif 'manager' in title.lower():
+        df.loc[i,'job_simp'] = 'manager'
+    
+    #Director
+    elif 'director' in title.lower():
+        df.loc[i,'job_simp'] = 'director'
+    
+    else:
+        df.loc[i,'job_simp'] = np.nan
+        
+        
+    if 'sr' in title.lower() or 'senior' in title.lower() or 'lead' in title.lower() or 'sr.' in title.lower() or 'principal' in title.lower():
+        df.loc[i,'seniority'] = 'sr'
+
+    elif 'jr' in title.lower() or 'jr.' in title.lower():
+         df.loc[i,'seniority'] = 'jr'
+    
+    else:
+         df.loc[i,'seniority'] = np.nan
+        
+        
+
+#Job Description
+df['desc_len'] = df['Job Description'].apply(lambda x: len(x))
+
+
+
+#Competitor count
+df['num_comp'] = df['Competitors'].apply(lambda x: len(x.split(',')) if x != '-1' else np.nan)
+
+
+
+#company text
+df['company_txt']=df.company_txt.apply(lambda x: x.strip())
+
+
 
 #Saving the Data
 df.to_csv('processed_data.csv')
